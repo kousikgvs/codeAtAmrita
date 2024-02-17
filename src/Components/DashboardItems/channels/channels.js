dcbee7245286b3a4984741edad5f68f675eb0687
Kousikgvs <kousikgvs@gmail.com>
2024-02-17 18:22:31 +0530

    Added Channel Divs

--- src/Components/DashboardItems/channels/channels.js
+++ src/Components/DashboardItems/channels/channels.js
@@ -1,27 +1,31 @@
 import React from 'react'
 import ChannelItem from './ChannelItem'
 import "./Channels.css"
-import { useEffect , useState } from 'react'
+import { useEffect, useState } from 'react'
 const Channels = () => {
-    const [parsedData, setParsedData] = useState({});
+  const [parsedData, setParsedData] = useState({});
 
-useEffect(() => {
+  useEffect(() => {
     const storedData = localStorage.getItem('user-data');
     if (storedData) {
       setParsedData(JSON.parse(storedData));
     }
   }, [localStorage.getItem('user-data')]);
 
-  const isLoggedIn = !!parsedData.email; 
+  const isLoggedIn = !!parsedData.email;
   console.log(parsedData.email)
 
   return (
     <div className='channels'>
-        <p className='channels-heading'>Channels Here</p>
-        <div className='channels-div'>
-            <ChannelItem branch={"IT Branches"} image={"https://cdn-icons-png.flaticon.com/512/2818/2818072.png"} description=""/>
-            <ChannelItem branch={"Non IT branches"} image={"https://cdn-icons-png.flaticon.com/512/7991/7991055.png"} description="" />
-        </div>
+      <p className='channels-heading'>Channels Here</p>
+      {
+        isLoggedIn ?
+          <div className='channels-div'>
+            <ChannelItem branch={"Beginner"} image={"https://cdn-icons-png.flaticon.com/512/2818/2818072.png"} description="Perfect for those who are new to the topic and want to start learning from scratch and starting to code." />
+            <ChannelItem branch={"Expert"} image={"https://cdn-icons-png.flaticon.com/512/7991/7991055.png"} description="Advanced content tailored for experts in the field who are looking for in-depth discussions and insights." />
+          </div>
+          : <>Please Login to Continue</>
+      }
     </div>
   )
 }
